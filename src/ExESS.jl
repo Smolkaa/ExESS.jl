324f3001ed5247c471bea0ee15feb98ee02fc78b
Alexander Smolka <alexander.smolka@tum.de>
2024-03-02 10:45:50 +0100

    file formatting update

--- src/ExESS.jl
+++ src/ExESS.jl
@@ -1,7 +1,9 @@
 module ExESS
 
 
-#::. loading packages
+############################################################################################
+#::. LOADING PACKAGES
+############################################################################################
 using DelimitedFiles: readdlm
 using DifferentialEquations: ODESolution, SecondOrderODEProblem, solve, terminate!, Tsit5, 
                              VectorContinuousCallback
@@ -13,19 +15,21 @@ using StaticArrays: SA
 using Statistics
 
 
-#::. BASE
+############################################################################################
+#::. LOADING SOURCES
+############################################################################################
+# base
 include(joinpath(@__DIR__, "base", "constants.jl"))
 include(joinpath(@__DIR__, "base", "coordinates.jl"))
 include(joinpath(@__DIR__, "base", "statistics.jl"))
 include(joinpath(@__DIR__, "base", "utils.jl"))
 
-#::. DRIVERS
+# drivers
 include(joinpath(@__DIR__, "drivers", "maxwellian.jl"))
 include(joinpath(@__DIR__, "drivers", "surface_distributions.jl"))
 include(joinpath(@__DIR__, "drivers", "thermal_sorption.jl"))
 
-
-#::. GRIDS
+# grids
 include(joinpath(@__DIR__, "grids", "grids.jl"))
 include(joinpath(@__DIR__, "grids", "spherical2d.jl"))
 include(joinpath(@__DIR__, "grids", "spherical2d_spiral.jl"))
@@ -33,34 +37,34 @@ include(joinpath(@__DIR__, "grids", "spherical2d_HEALPix.jl"))
 include(joinpath(@__DIR__, "grids", "spherical3d.jl"))
 include(joinpath(@__DIR__, "grids", "cartesian3d.jl"))
 
-
-#::. TRAJECTORY
+# trajectory
 include(joinpath(@__DIR__, "trajectories", "trajectory.jl"))
 include(joinpath(@__DIR__, "trajectories", "escape_velocity.jl"))
 include(joinpath(@__DIR__, "trajectories", "scale_height.jl"))
 
-#::. CERES
+# ceres
 include(joinpath(@__DIR__, "ceres", "constants.jl"))
 
-#::. MERCURY
+# mercury
 include(joinpath(@__DIR__, "mercury", "constants.jl"))
 
-#::. MOON
+# moon
 include(joinpath(@__DIR__, "moon", "constants.jl"))
 include(joinpath(@__DIR__, "moon", "lunar_surface_temperatures_DIVINER.jl"))
 include(joinpath(@__DIR__, "moon", "lunar_surface_temperatures_BUTLER1997.jl"))
 include(joinpath(@__DIR__, "moon", "lunar_surface_temperatures_HURLEY2015.jl"))
 
-#::. MISC
+# misc
 include(joinpath(@__DIR__, "misc", "projection_CHAMBERLAIN1963.jl"))
 include(joinpath(@__DIR__, "misc", "solar_angle.jl"))
 
 
 
 
+############################################################################################
 """
     ExESS
 """
 ExESS
 
-end # module
+end
