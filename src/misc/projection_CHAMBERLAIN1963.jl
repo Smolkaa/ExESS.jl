e82f1574eaabad07f5a882083f85b9a137f9f3a6
Alexander Smolka <a.smolka@tum.de>
2024-04-01 17:45:54 +0200

    update Chamberlain (1963) projection

--- src/misc/projection_CHAMBERLAIN1963.jl
+++ src/misc/projection_CHAMBERLAIN1963.jl
@@ -1,6 +1,10 @@
+############################################################################################
 #::. FUNCTIONS
+# TODO: Something does not add up here. Very close projections are exceeding 100%. Write
+#       a test to check the implementation. Check publication again.
+############################################################################################
 """
-    [1] projection_CHAMBERLAIN1963(n1::Real, r1::Real, r2::Real, T::Real, m::Real; 
+    [1] projection_CHAMBERLAIN1963(r1::Real, r2::Real, T::Real, m::Real; 
                                    M::Real=LUNAR_MASS, bal::Bool=true, sat::Bool=true, 
                                    esc::Bool=true)
 
@@ -17,9 +21,9 @@ equation (14) in Chamberlain (1963).
 | Field    | Default Value  | Unit       | Description                          |
 |:-------- | --------------:|:---------- |:------------------------------------ |
 | `M`      | `LUNAR_MASS`   | (kg)       | mass of central object               |
-| `bal`    | `true`         |            | include ballistic particles          |
-| `sat`    | `true`         |            | include satellite particles          |
-| `esc`    | `true`         |            | include escaping particles           |
+| `bal`    | `true`         | ()         | include ballistic particles          |
+| `sat`    | `true`         | ()         | include satellite particles          |
+| `esc`    | `true`         | ()         | include escaping particles           |
 
 
 **Rerences**
@@ -28,29 +32,56 @@ equation (14) in Chamberlain (1963).
 - Cook et al. 2013, "New upper limits on numerous atmospheric species in the native lunar
   atmosphere"
 """
-function projection_CHAMBERLAIN1963(n1::Real, r1::Real, r2::Real, T::Real, m::Real; 
+function projection_CHAMBERLAIN1963(r1::Real, r2::Real, T::Real, m::Real; 
                                     M::Real=LUNAR_MASS, bal::Bool=true, sat::Bool=true, 
                                     esc::Bool=true)
-    pot_energy_1 = GRAVITATIONAL_CONSTANT * M * m / (BOLTZMANN_CONSTANT * T * r1)
-    pot_energy_2 = GRAVITATIONAL_CONSTANT * M * m / (BOLTZMANN_CONSTANT * T * r2)
-    
-    # pre-calculations
-    L = pot_energy_1^2 / (pot_energy_1 + pot_energy_2)
-    g =  gamma(1.5)
-    gi = gamma_inc(1.5, pot_energy_1)[1] * g
-    giL = gamma_inc(1.5, pot_energy_1 - L)[1] * g
-    f = sqrt(pot_energy_2^2 - pot_energy_1^2) / pot_energy_2
+    if r1 > r2
+		pot_energy_1 = GRAVITATIONAL_CONSTANT * M * m / (BOLTZMANN_CONSTANT * T * r1)
+		pot_energy_2 = GRAVITATIONAL_CONSTANT * M * m / (BOLTZMANN_CONSTANT * T * r2)
+		
+		# pre-calculations
+		L = pot_energy_1^2 / (pot_energy_1 + pot_energy_2)
+		g =  gamma(1.5)
+		gi = gamma_inc(1.5, pot_energy_1)[1] * g
+		giL = gamma_inc(1.5, pot_energy_1 - L)[1] * g
+		f = sqrt(pot_energy_2^2 - pot_energy_1^2) / pot_energy_2
+	
+		# partition functions (ballistic, satellite, escape)
+		par = 0
+		if bal; par += 2/sqrt(pi) * (gi - f*exp(-L)*giL); end
+		if sat; par += 2/sqrt(pi) * (f*exp(-L)*giL); end
+		if esc; par += 1/sqrt(pi) * (g - gi - f*exp(-L)*(g - giL)); end
+	
+		# return projection
+		return exp(-(pot_energy_1 - pot_energy_2)) * par
 
-    # partition functions (ballistic, satellite, escape)
-    par = 0
-    if bal; par += 2/sqrt(pi) * (gi - f*exp(-L)*giL); end
-    if sat; par += 2/sqrt(pi) * (f*exp(-L)*giL); end
-    if esc; par += 1/sqrt(pi) * (g - gi - f*exp(-L)*(g - giL)); end
+	elseif r1 < r2
+		pot_energy_1 = GRAVITATIONAL_CONSTANT * M * m / (BOLTZMANN_CONSTANT * T * r2)
+		pot_energy_2 = GRAVITATIONAL_CONSTANT * M * m / (BOLTZMANN_CONSTANT * T * r1)
+		
+		# pre-calculations
+		L = pot_energy_1^2 / (pot_energy_1 + pot_energy_2)
+		g =  gamma(1.5)
+		gi = gamma_inc(1.5, pot_energy_1)[1] * g
+		giL = gamma_inc(1.5, pot_energy_1 - L)[1] * g
+		f = sqrt(pot_energy_2^2 - pot_energy_1^2) / pot_energy_2
+	
+		# partition functions (ballistic, satellite, escape)
+		par = 0
+		if bal; par += 2/sqrt(pi) * (gi - f*exp(-L)*giL); end
+		if sat; par += 2/sqrt(pi) * (f*exp(-L)*giL); end
+		if esc; par += 1/sqrt(pi) * (g - gi - f*exp(-L)*(g - giL)); end
+	
+		# return projection
+		return exp(pot_energy_1 - pot_energy_2) / par
+	end
 
-    return n1 * exp(-(pot_energy_1 - pot_energy_2)) * par
+	return 1
 end
 
 
+############################################################################################
 #::. EXPORTS
+############################################################################################
 export 
     projection_CHAMBERLAIN1963
