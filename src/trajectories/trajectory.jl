63669ac1744a6f5cde0cfbbca4471937da333c10
Alexander Smolka <a.smolka@tum.de>
2024-04-25 07:08:14 +0200

    housekeeping `trajectories.jl`

--- src/trajectories/trajectory.jl
+++ src/trajectories/trajectory.jl
@@ -27,7 +27,6 @@ end of the time span is reached.
 **Notes**
 
 * 16-bit types are not accurate enough and will be promoted (no type-stability)
-* calling the function with `<:Integer` arguments will promote them to `Float64`
 """
 function trajectory(x0::NTuple{3, T}, v0::NTuple{3, T}, ddx::Function=ddx_gravity;
         alg=Tsit5(), rmin::Real=LUNAR_RADIUS, rmax::Real=1e9, tspan::Tuple=(0f0,1f10),
@@ -36,7 +35,6 @@ function trajectory(x0::NTuple{3, T}, v0::NTuple{3, T}, ddx::Function=ddx_gravit
     x0 = SA{T}[x0 ./ norm(x0) .* (rmin + 0.1) ...] # normalize starting position to surface
     v0 = SA[v0...]
 
-    # function _f!(ddu, du, u, p, t); ddu .= SA[ddx(u, du)...]; end
     function _f(du, u, p, t); return SA[ddx(u, du)...]; end
     prob = SecondOrderODEProblem(_f, v0, x0, tspan)
 
