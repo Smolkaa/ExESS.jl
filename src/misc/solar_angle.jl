d51c68e05b854dba4b0c6109e387d2104483c772
Alexander Smolka <a.smolka@tum.de>
2024-05-18 15:28:06 +0200

    added additional functions for type stability for `solar_angle` and `scale_height`

--- src/misc/solar_angle.jl
+++ src/misc/solar_angle.jl
@@ -1,4 +1,6 @@
+############################################################################################
 #::. FUNCTIONS
+############################################################################################
 """
     [1] solar_angle(theta::Real, phi::Real)
     [2] solar_angle(xs::GlobalSphericalPosition)
@@ -8,12 +10,15 @@ Calculate the solar incidence angle based in selenocentric coordinates given
 as `theta` and `phi` in radians. Providing a `grid` as the input argument automatically
 extracts the angular coordinates and returns all solar angles as a vector.
 """
-solar_angle(theta::Real, phi::Real) = acos(cos(theta) * cos(phi))
+solar_angle(theta::S, phi::S) where {S<:AbstractFloat} = acos(cos(theta) * cos(phi))
+solar_angle(theta::Integer, phi::Integer) = solar_angle(float(theta), float(phi))
+solar_angle(args...) = solar_angle(promote(args...)...)
 solar_angle(xs::GlobalSphericalPosition) = solar_angle(xs.theta, xs.phi)
 solar_angle(x::AbstractPosition) = solar_angle(GlobalSphericalPosition(x))
 solar_angle(grid::AbstractGrid) = [solar_angle(c.theta, c.phi) for c in grid.coords]
 
 
+############################################################################################
 #::. EXPORTS
+############################################################################################
 export solar_angle
-
--- src/trajectories/scale_height.jl
+++ src/trajectories/scale_height.jl
@@ -1,6 +1,8 @@
+############################################################################################
 #::. FUNCTIONS
+############################################################################################
 """
-    [1] scale_height(T, m::Real; R::Real=LUNAR_RADIUS, M::Real=LUNAR_MASS)
+    [1] scale_height(T::Real, m::Real; R::Real=LUNAR_RADIUS, M::Real=LUNAR_MASS)
 
 Calculates the scale height of a particle with mass `m`, given the thermal energy
 based on the thermodynamic equilibrium temperature `T`, in the exosphere of a planetary
@@ -8,10 +10,15 @@ body with radius `R` and mass `M`.
 
 Default values for the planetary body are corresponding to the Moon.
 """
-function scale_height(T::Real, m::Real; R::Real=LUNAR_RADIUS, M::Real=LUNAR_MASS)
-    return BOLTZMANN_CONSTANT * T * R^2 / (GRAVITATIONAL_CONSTANT * m * M)
+function scale_height(T::S, m::S;
+                      R::Real=LUNAR_RADIUS, M::Real=LUNAR_MASS) where {S<:AbstractFloat}
+    return S(BOLTZMANN_CONSTANT * T * R^2 / (GRAVITATIONAL_CONSTANT * m * M))
 end
+scale_height(T::Real, m::Real; kwargs...) = scale_height(promote(T, m)...; kwargs...)
+scale_height(T::Integer, m::Integer; kwargs...) = scale_height(float(T),float(m); kwargs...)
 
 
+############################################################################################
 #::. EXPORTS
+############################################################################################
 export scale_height
