7c4d85854966be99a09d0f6779054705ad70f841
Alexander Smolka <alexander.smolka@tum.de>
2024-07-04 15:11:06 +0200

    update sphericl2d grids with new pclamp method for input arguments + adjusted tests

--- src/grids/spherical2d.jl
+++ src/grids/spherical2d.jl
@@ -37,7 +37,7 @@ function Spherical2DGrid(T::Type{<:AbstractFloat}, r::Real, N_theta::Integer, N_
     phi = T.(repeat(range(1/N_phi-1, 1-1/N_phi, length=N_phi) * pi/2, outer=N_theta))
     dtheta, dphi = theta[N_phi+1] - theta[1], phi[2] - phi[1]
     areas = [r^2 * dtheta * (sin(p+dphi/2) - sin(p-dphi/2)) for p in phi]
-    return Spherical2DGrid{T}(T(r), N_theta, N_phi, 
+    return Spherical2DGrid{T}(T(r), N_theta, N_phi,
                               GlobalSphericalPosition.(T(r), theta, phi), areas)
 end
 function Spherical2DGrid(r::Real, N_theta::Integer, N_phi::Integer)
@@ -78,7 +78,7 @@ function Spherical2DGrid_EqSim(T::Type{<:AbstractFloat}, r::Real, N_theta::Integ
     phi = T.(repeat(range(1/N_phi, 2-1/N_phi, length=N_phi) * pi/4, outer=N_theta))
     dtheta, dphi = theta[N_phi+1] - theta[1], phi[2] - phi[1]
     areas = [r^2 * dtheta * (sin(p+dphi/2) - sin(p-dphi/2)) for p in phi]
-    return Spherical2DGrid_EqSim{T}(T(r), N_theta, N_phi, 
+    return Spherical2DGrid_EqSim{T}(T(r), N_theta, N_phi,
                                     GlobalSphericalPosition.(T(r), theta, phi), areas)
 end
 function Spherical2DGrid_EqSim(r::Real, N_theta::Integer, N_phi::Integer)
@@ -94,7 +94,7 @@ end
     [2] Spherical2DGrid_Reduced([T::Type,] r::Real, N_phi::Integer)
 
 Global structured grid of surface coordinates (2D) of type `GlobalSphericalPosition{T}` over
-the sphere radius `r`. The grid is reduced in the azimuth direction to have approximately 
+the sphere radius `r`. The grid is reduced in the azimuth direction to have approximately
 equal `2*pi*r*cos(phi)/N_theta` grid element lengths.
 
 | Field     | Type; with `T<:AbstractFloat`        | Description                       |
@@ -118,7 +118,7 @@ end
 function Spherical2DGrid_Reduced(T::Type{<:AbstractFloat}, r::Real, N_phi::Integer)
     dtheta_max = T(pi/N_phi)
     phi0 = T.(range(1/N_phi-1, 1-1/N_phi, length=N_phi) * pi/2)
-    
+
     theta, phi, N_theta = T[], T[], Int64[]
     for p0 in phi0
         n_theta = ceil(Int64, 2*pi*cos(p0)/dtheta_max)
@@ -134,13 +134,13 @@ function Spherical2DGrid_Reduced(T::Type{<:AbstractFloat}, r::Real, N_phi::Integ
         push!(areas, repeat([r^2 * dtheta * (sin(phi0[i]+dphi/2) - sin(phi0[i]-dphi/2))], N_theta[i])...)
     end
 
-    return Spherical2DGrid_Reduced(T(r), N_theta, N_phi, 
+    return Spherical2DGrid_Reduced(T(r), N_theta, N_phi,
                                    GlobalSphericalPosition.(T(r), theta, phi), areas)
 end
 function Spherical2DGrid_Reduced(r::Real, N_phi::Integer)
     return Spherical2DGrid_Reduced(typeof(r), r, N_phi)
 end
-function Spherical2DGrid_Reduced(r::Integer, N_phi::Integer) 
+function Spherical2DGrid_Reduced(r::Integer, N_phi::Integer)
     return Spherical2DGrid_Reduced(promote_type(typeof(r), Float64), r, N_phi)
 end
 
@@ -151,7 +151,7 @@ end
 
 Global structured grid of surface coordinates (2D) of type `GlobalSphericalPosition{T}` over
 the upper hemisphere with radius `r`, assuming equatorial symmetry. The grid is reduced in
-the azimuth direction to have approximately equal `2*pi*r*cos(phi)/N_theta` grid element 
+the azimuth direction to have approximately equal `2*pi*r*cos(phi)/N_theta` grid element
 lengths.
 
 | Field     | Type; with `T<:AbstractFloat`        | Description                       |
@@ -175,7 +175,7 @@ end
 function Spherical2DGrid_Reduced_EqSim(T::Type{<:AbstractFloat}, r::Real, N_phi::Integer)
     dtheta_max = T(pi/2/N_phi)
     phi0 = T.(range(1/N_phi, 2-1/N_phi, length=N_phi) * pi/4)
-    
+
     theta, phi, N_theta = T[], T[], Int64[]
     for p0 in phi0
         n_theta = ceil(Int64, 2*pi*cos(p0)/dtheta_max)
@@ -191,10 +191,10 @@ function Spherical2DGrid_Reduced_EqSim(T::Type{<:AbstractFloat}, r::Real, N_phi:
         push!(areas, repeat([r^2 * dtheta * (sin(phi0[i]+dphi/2) - sin(phi0[i]-dphi/2))], N_theta[i])...)
     end
 
-    return Spherical2DGrid_Reduced_EqSim(T(r), N_theta, N_phi, 
+    return Spherical2DGrid_Reduced_EqSim(T(r), N_theta, N_phi,
                                          GlobalSphericalPosition.(T(r), theta, phi), areas)
 end
-function Spherical2DGrid_Reduced_EqSim(r::Real, N_phi::Integer) 
+function Spherical2DGrid_Reduced_EqSim(r::Real, N_phi::Integer)
     return Spherical2DGrid_Reduced_EqSim(typeof(r), r, N_phi)
 end
 function Spherical2DGrid_Reduced_EqSim(r::Integer, N_phi::Integer)
@@ -206,30 +206,30 @@ end
 #::. UTILITY FUNCTIONS
 ############################################################################################
 function coord2idx(grid::Spherical2DGrid, theta::Real, phi::Real)
-    theta = theta == -pi ? pi : theta
-    phi = phi == -pi/2 ? -pi/2+eps(Float64) : phi
+    theta = pclamp(theta, -pi + eps(Float64), pi)
+    phi = clamp(phi, -pi/2 + eps(Float64), pi/2)
     idxtheta = ceil(Int64, (theta+pi)*grid.N_theta/2/pi)
     idxphi = ceil(Int64, (phi+pi/2)*grid.N_phi/pi)
     return (idxtheta-1) * grid.N_phi + idxphi
 end
 function coord2idx(grid::Spherical2DGrid_EqSim, theta::Real, phi::Real)
-    theta = theta == -pi ? pi : theta
-    phi = phi == 0 ? eps(Float64) : phi
+    theta = pclamp(theta, -pi + eps(Float64), pi)
+    phi = clamp(phi, eps(Float64), pi/2)
     idxtheta = ceil(Int64, (theta+pi)*grid.N_theta/2/pi)
     idxphi = ceil(Int64, abs(phi)*grid.N_phi*2/pi)
     return (idxtheta-1) * grid.N_phi + idxphi
 end
 function coord2idx(grid::Spherical2DGrid_Reduced, theta::Real, phi::Real)
-    theta = theta == -pi ? pi : theta
-    phi = phi == -pi/2 ? -pi/2+eps(Float64) : phi
+    theta = pclamp(theta, -pi + eps(Float64), pi)
+    phi = clamp(phi, -pi/2 + eps(Float64), pi/2)
     idxphi = ceil(Int64, (phi+pi/2)/pi*grid.N_phi)
     idxtheta = ceil(Int64, (theta+pi)/2/pi*grid.N_theta[idxphi])
     if idxphi == 1; return idxtheta; end
     return idxtheta + accumulate(+, grid.N_theta[1:idxphi])[end-1]
 end
 function coord2idx(grid::Spherical2DGrid_Reduced_EqSim, theta::Real, phi::Real)
-    theta = theta == -pi ? pi : theta
-    phi = phi == 0 ? eps(Float64) : phi
+    theta = pclamp(theta, -pi + eps(Float64), pi)
+    phi = clamp(phi, eps(Float64), pi/2)
     idxphi = ceil(Int64, abs(phi)*grid.N_phi*2/pi)
     idxtheta = ceil(Int64, (theta+pi)*grid.N_theta[idxphi]/2/pi)
     if idxphi == 1; return idxtheta; end
@@ -255,7 +255,7 @@ Base.length(grid::AbstractSpherical2DGrid) = length(grid.coords)
 Base.size(grid::AbstractSpherical2DGrid) = (grid.N_theta, grid.N_phi)
 
 
-Base.show(io::IO, ::MIME"text/plain", grid::AbstractSpherical2DGrid) = 
+Base.show(io::IO, ::MIME"text/plain", grid::AbstractSpherical2DGrid) =
     print(io, "$(typeof(grid)):\n"*
             " r:       $(grid.r)\n"*
             " N_theta: $(grid.N_theta)\n"*
@@ -265,7 +265,7 @@ Base.show(io::IO, ::MIME"text/plain", grid::AbstractSpherical2DGrid) =
             "            min: $(min(grid.areas...))\n"*
             "            max: $(max(grid.areas...))\n")
 
-Base.show(io::IO, ::MIME"text/plain", grid::Spherical2DGrid_Reduced) = 
+Base.show(io::IO, ::MIME"text/plain", grid::Spherical2DGrid_Reduced) =
     print(io, "$(typeof(grid)):\n"*
             " r:       $(grid.r)\n"*
             " N_theta: $(length(grid.N_theta))-element Vector{Int64}\n"*
@@ -277,7 +277,7 @@ Base.show(io::IO, ::MIME"text/plain", grid::Spherical2DGrid_Reduced) =
             "            min: $(min(grid.areas...))\n"*
             "            max: $(max(grid.areas...))\n")
 
-Base.show(io::IO, ::MIME"text/plain", grid::Spherical2DGrid_Reduced_EqSim) = 
+Base.show(io::IO, ::MIME"text/plain", grid::Spherical2DGrid_Reduced_EqSim) =
     print(io, "$(typeof(grid)):\n"*
             " r:       $(grid.r)\n"*
             " N_theta: $(length(grid.N_theta))-element Vector{Int64}\n"*
@@ -287,14 +287,14 @@ Base.show(io::IO, ::MIME"text/plain", grid::Spherical2DGrid_Reduced_EqSim) =
             " coords:  $(length(grid.coords))-element $(typeof(coords(grid)))\n"*
             " areas:   $(length(grid.areas))-element $(typeof(grid.areas))\n"*
             "            min: $(min(grid.areas...))\n"*
-            "            max: $(max(grid.areas...))\n") 
+            "            max: $(max(grid.areas...))\n")
 
-            
+
 ############################################################################################
 #::. EXPORTS
 ############################################################################################
-export 
+export
     Spherical2DGrid,
-    Spherical2DGrid_EqSim, 
+    Spherical2DGrid_EqSim,
     Spherical2DGrid_Reduced,
     Spherical2DGrid_Reduced_EqSim
