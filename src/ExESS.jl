60faa1216343885a04c0cbd6dacee9921e759c81
Alexander Smolka <alexander.smolka@tum.de>
2024-08-23 16:51:47 +0200

    edited ExESS module docstring

--- src/ExESS.jl
+++ src/ExESS.jl
@@ -64,7 +64,32 @@ include(joinpath(@__DIR__, "misc", "solar_angle.jl"))
 
 ############################################################################################
 """
-    ExESS
+    ExESS.jl
+
+## Extraterrestrial Exosphere and Surface Simulations
+
+Scientific code library for simulating surface-bounded exosphere environments of airless
+bodies in the solar system. Note that the package is under development and subject to
+frequent changes. For questions, please contact the A. Smolka via email.
+
+The documentation and manual can be found in the [GitHub wiki](https://github.com/Smolkaa/ExESS.jl/wiki).
+
+_Author: A. Smolka (a.smolka@tum.de)_
+
+## Installation & Usage
+
+Please refer to the [official guide](https://julialang.org/downloads/platform/) to
+installing Julia on your machine. The `ExESS.jl` package can be installed using Julia's
+package-manager `Pkg`:
+```julia
+using Pkg
+Pkg.add("https://github.com/Smolkaa/ExESS.jl.git")
+using ExESS
+```
+If you want to use the latest development version, you can install the package as:
+```julia
+Pkg.add("https://github.com/Smolkaa/ExESS.jl.git#dev")
+```
 """
 ExESS
 
