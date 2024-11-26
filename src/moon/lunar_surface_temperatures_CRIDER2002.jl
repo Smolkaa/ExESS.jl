b238a0dcccb6e84c16f9847e0c1db67f117bc3ba
Alexander Smolka <alexander.smolka@tum.de>
2024-11-26 18:04:56 +0100

    housekeeping of docstring and methods for `lunar_surface_temperatures_CRIDER2002` and added type stability and behavior tests

--- src/moon/lunar_surface_temperatures_CRIDER2002.jl
+++ src/moon/lunar_surface_temperatures_CRIDER2002.jl
@@ -9,15 +9,17 @@
 Calculates the lunar surface temperatures based on the analytic formula given in Crider et
 al. 2002. The input is given either as the solar zenith angle `x` in radians, or a sub-solar
 position `lon` and `lat` in radians. Both approaches can be used with vectorized inputs as
-well as a grid of points defined in `grid`. Additionally, the user can provide a type `S` to
-convert the output to the desired type.
+well as a grid of points defined in `grid`. Note that only the surface grids points are
+used for the surface temperature calculation (see `surfacecoords`). Additionally, the user
+can provide a type `S` to convert the output to the desired type.
 
 # Arguments
 - (optional) `S::Type{<:AbstractFloat}`: Output type.
-- `lon::Real` or `lon::AbstractVector`: Longitude(s) in the range (-π, π).
-- `lat::Real` or `lat::AbstractVector`: Latitude(s) in the range (-π/2, π/2).
+- 'x::Real': Solar zenith angle in the range (-π, π). OR
 - `x::GlobalSphericalPosition` or `x::Tuple{Real, Real, Real}` or `x::AbstractVector` or
   an `Abstractvector` with entries of the same: SSE coordinate(s).
+- `lon::Real` or `lon::AbstractVector`: Longitude(s) in the range (-π, π).
+- `lat::Real` or `lat::AbstractVector`: Latitude(s) in the range (-π/2, π/2).
 - `grid::AbstractGrid`: Grid of points to evaluate the temperatures.
 
 # Key-Word Arguments (Based on Crider et al., 2002)
@@ -26,7 +28,7 @@ convert the output to the desired type.
   2019, this value is assumed to be 250 K.
 
 # Reference(s)
-- Butler (1997), The migration of volatiles on the surfaces of Mercury and the Moon.
+- Butler (1997). The migration of volatiles on the surfaces of Mercury and the Moon.
   DOI: 10.1029/97JE01347
 - Crider, D., & Vondrak, R. (2002). Hydrogen migration to the lunar poles by solar wind
   bombardment of the Moon. Advances in Space Research, 30(8), 1869-1874.
@@ -41,7 +43,10 @@ function lunar_surface_temperatures_CRIDER2002(x::Integer; kwargs...)
     return lunar_surface_temperatures_CRIDER2002(float(x); kwargs...)
 end
 function lunar_surface_temperatures_CRIDER2002(X::AbstractVector; kwargs...)
-    return lunar_surface_temperatures_CRIDER2002.(X; kwargs...)
+    return lunar_surface_temperatures_CRIDER2002.(X; kwargs...)inc
+end
+function lunar_surface_temperatures_CRIDER2002(grid::AbstractGrid; kwargs...)
+    return lunar_surface_temperatures_CRIDER2002(surfacecoords(grid); kwargs...)
 end
 function lunar_surface_temperatures_CRIDER2002(args...; kwargs...)
     return lunar_surface_temperatures_CRIDER2002(solar_angle(args...); kwargs...)
--- test/moon/temperatures/lunar_surface_temperatures_CRIDER2002.jl
+++ test/moon/temperatures/lunar_surface_temperatures_CRIDER2002.jl
@@ -1,7 +1,61 @@
 @testset verbose=true "lunar_surface_temperatures_CRIDER2002.jl" begin
 
-    #
-    grid = Spherical2DGrid(1, 720, 360)
-    @test lunar_surface_temperatures_CRIDER2002(grid) isa Vector{Float64}
+    ########################################################################################
+    #::. TYPE STABILITY
+    ########################################################################################
+    TYPES = [Int16, Int32, Int64, BigInt, Float16, Float32, Float64, BigFloat]
+    for t1 in TYPES, t3 in TYPES[5:end]
 
+        # test single input types (sza)
+        if t1 in TYPES[1:3]
+            @test all(x -> lunar_surface_temperatures_CRIDER2002(x) isa Float64, t1.(rand(-1:1, 1000)))
+            @test all(x -> lunar_surface_temperatures_CRIDER2002(t3, x) isa t3, t1.(rand(-1:1, 1000)))
+            @test lunar_surface_temperatures_CRIDER2002(t1.(rand(-1:1, 1000))) isa Vector{Float64}
+            @test lunar_surface_temperatures_CRIDER2002(t3, t1.(rand(-1:1, 1000))) isa Vector{t3}
+        elseif t1 == TYPES[4]
+            @test all(x -> lunar_surface_temperatures_CRIDER2002(x) isa BigFloat, t1.(rand(-1:1, 1000)))
+            @test all(x -> lunar_surface_temperatures_CRIDER2002(t3, x) isa t3, t1.(rand(-1:1, 1000)))
+            @test lunar_surface_temperatures_CRIDER2002(t1.(rand(-1:1, 1000))) isa Vector{BigFloat}
+            @test lunar_surface_temperatures_CRIDER2002(t3, t1.(rand(-1:1, 1000))) isa Vector{t3}
+        else
+            @test all(x -> lunar_surface_temperatures_CRIDER2002(x) isa t1, rand(t1, 1000))
+            @test all(x -> lunar_surface_temperatures_CRIDER2002(t3, x) isa t3, rand(t1, 1000))
+            @test lunar_surface_temperatures_CRIDER2002(rand(t1, 1000)) isa Vector{t1}
+            @test lunar_surface_temperatures_CRIDER2002(t3, rand(t1, 1000)) isa Vector{t3}
+        end
+
+        # test two input types (lon, lat)
+        for t2 in TYPES
+            tout = float(promote_type(t1, t2))
+            in1 = t1 <: Integer ? t1(rand(-1:1)) : rand(t1) .* pi/2
+            in2 = t2 <: Integer ? t2(rand(-1:1)) : rand(t2) .* pi/2
+
+            @test lunar_surface_temperatures_CRIDER2002(in1, in2) isa tout
+            @test lunar_surface_temperatures_CRIDER2002(t3, in1, in2) isa t3
+            @test lunar_surface_temperatures_CRIDER2002(repeat([in1],100), repeat([in2],100)) isa Vector{tout}
+            @test lunar_surface_temperatures_CRIDER2002(t3, repeat([in1],100), repeat([in2],100)) isa Vector{t3}
+        end
+    end
+
+    # test grid inputs
+    for t in TYPES[5:end]
+        grid = Spherical2DGrid(1, rand(10:100), rand(10:50))
+        @test lunar_surface_temperatures_CRIDER2002(grid) isa Vector{Float64}
+        @test lunar_surface_temperatures_CRIDER2002(t, grid) isa Vector{t}
+    end
+
+    ########################################################################################
+    #::. BEHAVIOR
+    ########################################################################################
+
+    # daytime temperature: lower for higher sza (at default kwargs)
+    @test all(x -> lunar_surface_temperatures_CRIDER2002(x) > lunar_surface_temperatures_CRIDER2002(1.1*x), rand(1000))
+
+    # nighttime temperature: constant
+    @test all(x -> lunar_surface_temperatures_CRIDER2002(x; T0=100) == 100, pi/2 .+ rand(1000))
+    @test all(T0 -> lunar_surface_temperatures_CRIDER2002(pi/2+0.1; T0=T0) == T0, rand(1000)*300)
+
+    # same output for sza and (lon, lat) inputs at same angular positions
+    @test all(x -> lunar_surface_temperatures_CRIDER2002(x) == lunar_surface_temperatures_CRIDER2002(0, x), rand(1000))
+    @test all(x -> lunar_surface_temperatures_CRIDER2002(x) == lunar_surface_temperatures_CRIDER2002(x, 0), rand(1000))
 end
