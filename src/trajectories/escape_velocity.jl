f99b13b036106aa7b1e98ebdb5afe97c3f3de40d
Alexander Peschel <a.peschel@tum.de>
2025-04-25 11:12:00 +0200

    adjusted the docstrings for escape velocity

--- src/trajectories/escape_velocity.jl
+++ src/trajectories/escape_velocity.jl
@@ -1,8 +1,12 @@
 #::. FUNCTIONS
 """
-    [1] escape_velocity(r::Real, m::Real)
+    escape_velocity(r, m)
 
 Calculates the magnitude of the escape velocity for an orbital radius `r` and body mass `m`.
+
+# Arguments
+- `r::Real`: Orbital radius in meters.
+- `m::Real`: Mass of the planetary body in kilograms.
 """
 escape_velocity(r::T, m::T) where {T<:AbstractFloat} = T(sqrt(2*GRAVITATIONAL_CONSTANT*m/r))
 escape_velocity(r::Real, m::Real) = escape_velocity(promote(r, m)...)
